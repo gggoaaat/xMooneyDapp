bc11eb8eccb94822cb0c906964c3285c8f40c440
Techoshi <risi@eminent-it.com>
2022-04-01 16:46:45 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     contractAddress: "0x98631c69602083d04f83934576a53e2a133d482f",
     infuraID: "b830c8484bf841d795848610ff791d5b",
     defaultGas: 210000,
-    debug: true,
+    debug: false,
     NEXT_PUBLIC_SERVER_URL: 'https://61xxdlusmokj.usemoralis.com:2053/server',
     NEXT_PUBLIC_APP_ID: 'FQ5GoOcQK7LvYmmP0faqvWaTiD0kDQLqSkGpJNWf'
   },
