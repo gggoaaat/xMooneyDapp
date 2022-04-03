5edfd4fae4d0ec6dc1ada56741b6e91ef6094e74
Techoshi <risi@eminent-it.com>
2022-04-03 03:08:38 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 // next.config.js
 module.exports = {
+  reactStrictMode: true,
+  analyticsId: '7vsHWbYwcKpA3lFTng2Qe9iNzE7',
+  env: {
+    contractAddress: "0x98631c69602083d04f83934576a53e2a133d482f",
+    infuraID: "b830c8484bf841d795848610ff791d5b",
+    defaultGas: 210000,
+    debug: false,
+    NEXT_PUBLIC_SERVER_URL: 'https://61xxdlusmokj.usemoralis.com:2053/server',
+    NEXT_PUBLIC_APP_ID: 'FQ5GoOcQK7LvYmmP0faqvWaTiD0kDQLqSkGpJNWf'
+  },
+  images: {
+    domains: ['static.wixstatic.com'],
+  },
   async headers() {
     return [
       {
@@ -14,22 +27,6 @@ module.exports = {
       }
     ]
   }
-};
-
-module.exports = {
-  reactStrictMode: true,
-  analyticsId: '7vsHWbYwcKpA3lFTng2Qe9iNzE7',
-  env: {
-    contractAddress: "0x98631c69602083d04f83934576a53e2a133d482f",
-    infuraID: "b830c8484bf841d795848610ff791d5b",
-    defaultGas: 210000,
-    debug: false,
-    NEXT_PUBLIC_SERVER_URL: 'https://61xxdlusmokj.usemoralis.com:2053/server',
-    NEXT_PUBLIC_APP_ID: 'FQ5GoOcQK7LvYmmP0faqvWaTiD0kDQLqSkGpJNWf'
-  },
-  images: {
-    domains: ['static.wixstatic.com'],
-  },
 }
 
 
