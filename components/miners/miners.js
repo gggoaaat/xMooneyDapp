2a64c336bca2aa2050de9685cd8de05ab0ce64b5
Techoshi <risi@eminent-it.com>
2022-04-03 03:57:17 -0400

    Working. Removing Excess

--- components/miners/miners.js
+++ components/miners/miners.js
@@ -40,11 +40,11 @@ export default function Miners(e) {
                     // mode: 'no-cors',
                     header: {
                         'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': '*',
-                        'Access-Control-Allow-Credentials': 'true',
-                        'Access-Control-Allow-Methods' : 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-                        'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-                // ?        'x-requested-with': window.location.host
+                //         'Access-Control-Allow-Origin': '*',
+                //         'Access-Control-Allow-Credentials': 'true',
+                //         'Access-Control-Allow-Methods' : 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                //         'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+                // // ?        'x-requested-with': window.location.host
                     }
                 };
                 try {
